2fe8d6dcadc8d6dffc68f23380d65ef26a6a637e
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2024-03-13 12:12:23 +0600

    color presets dynamic

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ const defaultPresetColors = [
 window.onload = () => {
   main();
   updateColorCodeToDom(defaultColor);
+  //Display preset colors
+  displayColorBoxes(
+    document.getElementById("preset-colors"),
+    defaultPresetColors
+  );
 };
 
 //Main or boot functions, this function will take care of getting all the DOM references
@@ -57,6 +62,7 @@ function main() {
   const colorSliderGreen = document.getElementById("color-slider-green");
   const colorSliderBlue = document.getElementById("color-slider-blue");
   const copyToClipboardBtn = document.getElementById("copy-to-clipboard");
+  const presetColorsParent = document.getElementById("preset-colors");
 
   //Event listeners
   generateRandomColorBtn.addEventListener(
@@ -80,6 +86,13 @@ function main() {
   );
 
   copyToClipboardBtn.addEventListener("click", handleCopyToClipboard);
+
+  presetColorsParent.addEventListener("click", function (e) {
+    const child = e.target;
+    if (child.className == "color-box") {
+      navigator.clipboard.writeText(child.getAttribute("data-color"));
+    }
+  });
 }
 
 //Event handlers
@@ -219,6 +232,32 @@ function updateColorCodeToDom(color) {
   document.getElementById("color-slider-green-label").innerText = color.green;
   document.getElementById("color-slider-blue").value = color.blue;
   document.getElementById("color-slider-blue-label").innerText = color.blue;
+}
+
+/**
+ * Generate color box with color-box class
+ * @param {string} color
+ * @returns {object}
+ */
+function generateColorBox(color) {
+  const div = document.createElement("div");
+  div.className = "color-box";
+  div.style.backgroundColor = color;
+  div.setAttribute("data-color", color);
+
+  return div;
+}
+
+/**
+ * Create and append new color boxes to it's parent
+ * @param {object} parent
+ * @param {Array} colors
+ */
+function displayColorBoxes(parent, colors) {
+  colors.forEach((color) => {
+    const colorBox = generateColorBox(color);
+    parent.appendChild(colorBox);
+  });
 }
 
 //Utils functions
